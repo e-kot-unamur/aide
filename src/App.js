cbe400fa3fa6414fe58424e2bcab77f6146af978
tsepton <tsepton@gmail.com>
2020-05-16 21:12:24 +0200

    Modify styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react'
-import './App.css'
+import './static/css/App.css'
 
+import logo from './static/images/logo.png'
 import Noeud from './component/Noeud'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         
+        <img src={logo} id="ekot-logo" alt="Logo de l'ekot" />
         <Noeud />
 
       </header>
