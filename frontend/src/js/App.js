11172a8b37eb89548c62affd708e7b8e6aef253c
tsepton <tsepton@gmail.com>
2020-05-17 20:27:24 +0200

    Remove unused imports

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,10 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import '../static/sass/App.scss'
 
 import logo from '../static/images/logo.png'
 import Stream from './page/Stream'
-
-const API_URL = "http://localhost:3001/api"
 
 function App() {
   return (
