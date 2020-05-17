ed0dc497a6ab9b4f96376fab28fd48598f8b92fd
tsepton <tsepton@gmail.com>
2020-05-17 16:28:42 +0200

    Configure front and back communication

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,23 +1,18 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import '../static/sass/App.scss'
 
 import logo from '../static/images/logo.png'
 import DiagramStream from './page/DiagramStream'
 
+const API_URL = "http://localhost:3001/api"
+
 function App() {
+  const [api, setApi] = useState("Api is not connected") //FIXME - Delete if working
 
   useEffect(() => {
-    console.log('Coucou')
-    fetch("localhost:5000")
-      .then(res => res.json())
-      .then(
-        (result) => {
-        console.log(result)
-      },
-        (error) => {
-        console.log(error)
-        }
-      )
+    fetch(API_URL)
+      .then(res => res.text())
+      .then(res => setApi(res), error => console.error(error))
   }, [])
 
   return (
@@ -25,6 +20,7 @@ function App() {
       <header className="App-header">
         <img src={logo} id="ekot-logo" alt="Logo de l'ekot" />
         <DiagramStream />
+        <span><b>{api}</b></span>
       </header>
     </div>
   );
