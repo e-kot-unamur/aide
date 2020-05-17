488e49db238277387baf54498f47c16e3ba0c776
tsepton <tsepton@gmail.com>
2020-05-17 19:58:47 +0200

    Remove api flag

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -7,20 +7,11 @@ import Stream from './page/Stream'
 const API_URL = "http://localhost:3001/api"
 
 function App() {
-  const [api, setApi] = useState("Api is not connected") //FIXME - Delete if working
-
-  useEffect(() => {
-    fetch(API_URL)
-      .then(res => res.text())
-      .then(res => setApi(res), error => console.error(error))
-  }, [])
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} id="ekot-logo" alt="Logo de l'ekot" />
         <Stream />
-        <span><b>{api}</b></span>
       </header>
     </div>
   );
