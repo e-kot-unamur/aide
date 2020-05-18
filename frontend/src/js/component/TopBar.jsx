bdda4d4c37199c93d75f62e0934b0f9760b12b79
tsepton <tsepton@gmail.com>
2020-05-18 21:59:09 +0200

    Add icons to links

--- frontend/src/js/component/TopBar.jsx
+++ frontend/src/js/component/TopBar.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
+import '../../static/sass/TopBar.scss'
+import faFacebook from '../../static/icons/facebook.svg'
+import faInfoCircle from '../../static/icons/info-circle.svg'
+
 //TODO - About
 function TopBar({ logo, width, ...props }) {
   return (
@@ -12,9 +16,11 @@ function TopBar({ logo, width, ...props }) {
       />
       <div className="top-bar-links">
         <a href="https://www.facebook.com/ekotnamur" target="_blank" rel="noopener noreferrer" className="top-bar-link">
+          <img src={faFacebook} className='icon' />
           <span>Prendre contact</span>
         </a>
         <a href="/" rel="noopener noreferrer" className="top-bar-link">
+          <img src={faInfoCircle} className='icon' />
           <span>A propos</span>
         </a>
       </div>
