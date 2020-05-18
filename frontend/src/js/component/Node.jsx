fdbcbe9ab83c69a4227258b5301501abd9477e7e
tsepton <tsepton@gmail.com>
2020-05-18 22:06:54 +0200

    Improve UI feeling

--- frontend/src/js/component/Node.jsx
+++ frontend/src/js/component/Node.jsx
@@ -3,36 +3,48 @@ import React from 'react'
 import { Card, ListGroup } from 'react-bootstrap';
 import '../../static/sass/Node.scss'
 
-function Node({ 
-    data,
-    handleAnswer,
-    handleReturn
- }) {
-    
-    return (
-        <Card variant='warning' className="node text-center">
-        <Card.Body>
-            {
-                data  
-                ? <Card.Text>{data.text}</Card.Text>
-                : <Card.Text>Loading</Card.Text>
-            }
-        </Card.Body>
-        <Card.Footer className="answers">
-            <ListGroup>
-                {
-                    data ? 
-                    data.answers.map(
-                        answer => <ListGroup.Item key={answer.ref} variant='primary' onClick={() => handleAnswer(answer.ref)}>{answer.text}</ListGroup.Item>) 
-                    : <></>
-                }
-                {
-                    <ListGroup.Item variant='warning' onClick={() => handleReturn()}>Retour</ListGroup.Item>
-                }
-            </ListGroup>
-        </Card.Footer>
-        </Card>
-    )
+function Node({
+  data,
+  handleAnswer,
+  handleReturn
+}) {
+
+  return (
+    <Card variant='warning' className="node text-center">
+      <Card.Body>
+        {
+          data
+            ? <Card.Text>{data.text}</Card.Text>
+            : <Card.Text>Loading</Card.Text>
+        }
+      </Card.Body>
+      <Card.Footer className="answers">
+        <ListGroup>
+          {
+            data ?
+              data.answers.map(
+                answer =>
+                  <ListGroup.Item
+                    key={answer.ref}
+                    variant='primary'
+                    onClick={() => handleAnswer(answer.ref)}>
+                    <a href="#" className='link'>
+                      {answer.text}
+                    </a>
+                  </ListGroup.Item>)
+              : <></>
+          }
+          {
+            <ListGroup.Item variant='warning' href="#" onClick={() => handleReturn()}>
+              <a href="#" className='link'>
+                Retour
+              </a>
+            </ListGroup.Item>
+          }
+        </ListGroup>
+      </Card.Footer>
+    </Card>
+  )
 }
 
 export default Node
