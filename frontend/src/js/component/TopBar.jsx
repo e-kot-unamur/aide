e2eac9801ef11b6ffd8510edc68b63bd12930379
tsepton <tsepton@gmail.com>
2020-05-18 23:12:18 +0200

    Add about modal

--- frontend/src/js/component/TopBar.jsx
+++ frontend/src/js/component/TopBar.jsx
@@ -1,30 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import '../../static/sass/TopBar.scss'
 import faFacebook from '../../static/icons/facebook.svg'
 import faInfoCircle from '../../static/icons/info-circle.svg'
 
-//TODO - About
+import { Modal } from 'react-bootstrap'
+
 function TopBar({ logo, width, ...props }) {
+  const [aboutShow, setAboutShow] = useState(false)
+
   return (
-    <div {...props}>
-      <img
-        className="top-bar-logo"
-        src={logo}
-        width={width}
-        alt="E-kot logo"
-      />
-      <div className="top-bar-links">
-        <a href="https://www.facebook.com/ekotnamur" target="_blank" rel="noopener noreferrer" className="top-bar-link">
-          <img src={faFacebook} className='icon' alt='facebook icone'/>
-          <span>Prendre contact</span>
-        </a>
-        <a href="/" rel="noopener noreferrer" className="top-bar-link">
-          <img src={faInfoCircle} className='icon' alt='A propos icone' />
-          <span>A propos</span>
-        </a>
+    <>
+      <div {...props}>
+        <img
+          className="top-bar-logo"
+          src={logo}
+          width={width}
+          alt="E-kot logo"
+        />
+        <div className="top-bar-links">
+          <a href="https://www.messenger.com/t/ekotnamur" target="_blank" rel="noopener noreferrer" className="top-bar-link">
+            <img src={faFacebook} className='icon' alt='facebook icone' />
+            <span>Prendre contact</span>
+          </a>
+          <a href="/#" rel="noopener noreferrer" className="top-bar-link" onClick={() => setAboutShow(true)}>
+            <img src={faInfoCircle} className='icon' alt='A propos icone' />
+            <span>A propos</span>
+          </a>
+        </div>
       </div>
-    </div>
+      <About
+        show={aboutShow}
+        onHide={() => setAboutShow(false)}
+      />
+    </>
+  )
+}
+
+function About({ ...props }) {
+  return (
+    <Modal
+      {...props}
+      size="md"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          A propos...
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <b>... de ce site</b>
+        <p>
+          Ce site a pour but de vous aider, via sa démarche intuitive,
+          en tentant de répondre aux questions les plus fréquentes
+          (et liées à l'informatique) des étudiants de l'UNamur.
+        </p>
+        <hr />
+        <b>... de l'E-kot</b>
+        <p>Il s'agit d'un kot à projet de l'UNamur visant à aider les étudiants rencontrant divers problèmes liés à l'informatique.</p>
+        <hr />
+        <h6 id='odc'>SUUUUUUUUUUU</h6>
+      </Modal.Body>
+    </Modal>
   )
 }
 
