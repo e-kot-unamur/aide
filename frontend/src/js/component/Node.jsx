9514834cf0e2fab445ade4a5834bbf342c808cd4
tsepton <tsepton@gmail.com>
2020-06-01 19:05:10 +0200

    Add style and full screen for image

--- frontend/src/js/component/Node.jsx
+++ frontend/src/js/component/Node.jsx
@@ -1,65 +1,117 @@
-import React, { useEffect } from 'react'
+import React, { useState } from 'react'
 
-import { Card, ListGroup } from 'react-bootstrap';
+import { Card, ListGroup, Button, Modal } from 'react-bootstrap';
 import '../../static/sass/Node.scss'
 
 function Node({
-  data,       
+  data,
   nodeId, // To get the image related to the node, if one
   getImgFromRemote,
   handleAnswer,
   handleReturn
 }) {
+  const [imgModal, setImgModal] = useState(false)
+
+  console.log(data)
 
   return (
-    <Card variant='warning' className="node text-center">
-      {
-        data.image && nodeId
-        ? <div className='illustration-container'>
-            <img className='illustration' 
-              variant='top' 
-              src={getImgFromRemote(nodeId)}
-              />
-          </div>
-        : <></>
-      }
-      <Card.Body>
+    <>
+      <Card variant='warning' className="node text-center">
         {
-          data.text
-            ? <Card.Text>{data.text}</Card.Text>
-            : <Card.Text>Loading</Card.Text>
+          data.image && nodeId
+            ? <div
+              className='illustration-container'
+              onClick={() => setImgModal(true)}
+            >
+              <img className='illustration'
+                variant='top'
+                alt='illustration'
+                src={getImgFromRemote(nodeId)}
+              />
+              <b className='illustration-text'>Cliquer pour aggrandir</b>
+            </div>
+            : <></>
         }
-      </Card.Body>
-      <Card.Footer className="answers">
-        <ListGroup>
+        <Card.Body>
           {
-            data.answers ?
-              data.answers.map(
-                answer =>
-                  <ListGroup.Item
-                    key={answer.ref}
-                    onClick={() => handleAnswer(answer.ref)}
-                    variant='primary'
-                    >
-                    <a href="/#" className='link'>
-                      {answer.text}
-                    </a>
-                  </ListGroup.Item>)
-              : <></>
+            data.text
+              ? <Card.Text>{data.text}</Card.Text>
+              : <Card.Text>Loading</Card.Text>
           }
-          <ListGroup.Item
-            className='bug-correction'
-            onClick={() => handleReturn()}
-            variant='warning'
-          >
-            <a href="/#" className='link'>
-              Retour
-            </a>
-          </ListGroup.Item>
-        </ListGroup>
-      </Card.Footer>
-    </Card>
+        </Card.Body>
+        <Card.Footer className="answers">
+          <ListGroup>
+            {
+              data.answers ?
+                data.answers.map(
+                  answer =>
+                    <ListGroup.Item
+                      key={answer.ref}
+                      onClick={() => handleAnswer(answer.ref)}
+                      variant='primary'
+                    >
+                      <a href="/#" className='link'>
+                        {answer.text}
+                      </a>
+                    </ListGroup.Item>)
+                : <></>
+            }
+            <ListGroup.Item
+              className='bug-correction'
+              onClick={() => handleReturn()}
+              variant='warning'
+            >
+              <a href="/#" className='link'>
+                Retour
+              </a>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card.Footer>
+      </Card>
+      {
+        data.image
+          ? <FullScreenImg
+            show={imgModal}
+            onHide={() => setImgModal(false)}
+            nodeId={nodeId}
+            caption={data.image.caption}
+            getImgFromRemote={getImgFromRemote}
+          />
+          : <></>
+      }
+    </>
   )
+}
+
+function FullScreenImg({
+  nodeId,
+  caption,
+  getImgFromRemote,
+  ...props
+}) {
+  return (
+    <Modal
+      {...props}
+      className='my-modal'
+      size="lg"
+      centered
+    >
+      <Modal.Header closeButton>
+        {
+          caption ? <b>{caption}</b> : <></>
+        }
+      </Modal.Header>
+      <Modal.Body>
+        <div className='full-screen-container'>
+          <img
+            className='full-screen-illustration'
+            alt='illustration'
+            src={getImgFromRemote(nodeId)}
+          />
+        </div>
+      </Modal.Body>
+    </Modal>
+  );
 }
 
 export default Node
