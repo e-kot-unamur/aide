4a94e5a11868120f4a1a5e9f57f5aed134a46c1e
tsepton <tsepton@gmail.com>
2020-06-08 19:07:24 +0200

    Add styling for feedback component

--- frontend/src/js/component/home/FeedbackSystem.jsx
+++ frontend/src/js/component/home/FeedbackSystem.jsx
@@ -1,28 +1,54 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
-import 'static/sass/component/home/FeedbackModal.scss'
-import { ReactComponent as FaGrinHeartsSolid } from 'static/icons/grin-hearts-solid.svg'
-import { ReactComponent as FaSadCrySolid } from 'static/icons/sad-cry-solid.svg'
+import React, { useState } from 'react'
+import { Card, InputGroup, FormControl, Button, Row, Col } from 'react-bootstrap'
+import 'static/sass/component/home/FeedbackSystem.scss'
 
 
 function FeedbackSystem({ show, ...props }) {
+  const [sadCry, setSadCry] = useState(false)
+  const [grinHearts, setGrinHearts] = useState(false)
+
+  const setSelection = (option) => {
+    if (option === 'first') {
+      setGrinHearts(false)
+      setSadCry(!sadCry)
+    } else {
+      setSadCry(false)
+      setGrinHearts(!grinHearts)
+    }
+  }
+
+  function sendFeedback() {
+    //TODO - send selection and comment to the API
+  }
+
   return (
-    <div show={show ? 'true' : 'false'} className='feedback-system'>
-      <Row>
-        <Col className='option-container'>
-          <FaSadCrySolid className='icon option' />
-          <p>
-            Non, malheureusement...
-          </p>
-        </Col>
-        <Col className='option-container'>
-          <FaGrinHeartsSolid className='icon option' />
-          <p>
-            Oui, merci beaucoup !
-          </p>
-        </Col>
-      </Row>
-    </div>
+    <div {...props} show-feedback={show ? 'true' : 'false'}>
+      <Card className='feedback-system'>
+        <Card.Body>
+          <Card.Title>Problème résolu ?</Card.Title>
+          <Card.Text>
+            <p>Votre avis nous intéresse !</p>
+          </Card.Text>
+          <Row>
+            <Col className={`option-container ${sadCry ? 'selected' : ''}`} onClick={() => setSelection('first')}>
+              <span className='sad-cry-solid option' />
+            </Col>
+            <Col className={`option-container ${grinHearts ? 'selected' : ''}`} onClick={() => setSelection('second')}>
+              <span className='grin-hearts-solid option' />
+            </Col>
+          </Row>
+        </Card.Body>
+        <Card.Footer>
+
+          <InputGroup>
+            <FormControl />
+            <InputGroup.Append>
+              <Button>Envoyer</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </Card.Footer>
+      </Card>
+    </div >
   )
 }
 
--- frontend/src/js/page/home/Diagram.jsx
+++ frontend/src/js/page/home/Diagram.jsx
@@ -1,66 +1,68 @@
 import React, { useState, useEffect } from 'react'
 
+import 'static/sass/page/home/Diagram.scss'
 import InnerNode from 'js/component/home/InnerNode'
 import FeedbackSystem from 'js/component/home/FeedbackSystem'
 import { url, endpoints } from 'js/lib'
 
 function Diagram({ title, nodes, setDiagram }) {
-    const [node, setNode] = useState({'id': 1, 'data': nodes[1]}) //Node 0 is for preview
-    const [history, setHistory] = useState([])
-    const [showFeedbackSystem, setShowFeedbackSystem] = useState(false) //FIXME
+  const [node, setNode] = useState({ 'id': 1, 'data': nodes[1] }) //Node 0 is for preview
+  const [history, setHistory] = useState([])
+  const [showFeedbackSystem, setShowFeedbackSystem] = useState(false)
 
-    // Checking for a root node
-    // Then ask for the user feedback
-    useEffect(() => {
-        const userFeedback = (v) => setTimeout(() => setShowFeedbackSystem(v), 500) //FIXME - Set a proper timeout
-        if (node.data.answers.length === 0) {
-            userFeedback(true)
-        } else {
-            userFeedback(false)
-        }
-    }, [node.data.answers])
-
-    // Change node according to the answer specified
-    const handleAnswer = (id) => {
-        if (nodes[id]) {
-            setHistory([...history, {'id':node.id, 'data':node.data}]) //Add actual node to the stack
-            setNode({'id': id, 'data': nodes[id]}) //Change the node to the next one 
-        } else {    
-            console.error('There is no node specified for the answer, check the json !')
-        }
+  // Checking for a root node
+  // Then ask for the user feedback
+  useEffect(() => {
+    const userFeedback = (v) => setTimeout(() => setShowFeedbackSystem(v), 5000) //FIXME - Set a proper timeout
+    if (node.data.answers.length === 0) {
+      userFeedback(true)
+    } else {
+      userFeedback(false)
     }
+  }, [node.data.answers])
 
-    // Back button
-    const handleReturn = () => {
-        if (history.length !== 0) {
-            let temp = history.slice()
-            let lastElement = temp.pop()
-            setHistory(temp)
-            setNode({'id': lastElement.id, 'data': lastElement.data})
-        } else {
-            setDiagram() // diagram === undefined, previews will be rerendered
-        }
+  // Change node according to the answer specified
+  const handleAnswer = (id) => {
+    if (nodes[id]) {
+      setHistory([...history, { 'id': node.id, 'data': node.data }]) //Add actual node to the stack
+      setNode({ 'id': id, 'data': nodes[id] }) //Change the node to the next one 
+    } else {
+      console.error('There is no node specified for the answer, check the json !')
     }
-    
-    // Give the URL where to find the img
-    const getImgFromRemote = (nodeNumber) => {
-        return url + endpoints.staticImages + `${title}/${nodeNumber}`
-    }
+  }
 
-    return (
-        <>
-        <InnerNode data={node.data}
-            handleAnswer={handleAnswer}
-            handleReturn={handleReturn}
-            nodeId={node.id}
-            getImgFromRemote={getImgFromRemote}
-        />
-        <FeedbackSystem 
-            show={showFeedbackSystem}
-            onHide={() => setShowFeedbackSystem(false)}
-        />
-        </>
-    )
+  // Back button
+  const handleReturn = () => {
+    if (history.length !== 0) {
+      let temp = history.slice()
+      let lastElement = temp.pop()
+      setHistory(temp)
+      setNode({ 'id': lastElement.id, 'data': lastElement.data })
+    } else {
+      setDiagram() // diagram === undefined, previews will be rerendered
+    }
+  }
+
+  // Give the URL where to find the img
+  const getImgFromRemote = (nodeNumber) => {
+    return url + endpoints.staticImages + `${title}/${nodeNumber}`
+  }
+
+  return (
+    <>
+      <InnerNode data={node.data}
+        handleAnswer={handleAnswer}
+        handleReturn={handleReturn}
+        nodeId={node.id}
+        getImgFromRemote={getImgFromRemote}
+      />
+      {
+        showFeedbackSystem
+          ? <FeedbackSystem className='center' />
+          : <></>
+      }
+    </>
+  )
 }
 
 export default Diagram
