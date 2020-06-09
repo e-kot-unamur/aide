a85ad1b20964e91452614df54a04bd51c30f6a52
tsepton <tsepton@gmail.com>
2020-06-10 00:25:31 +0200

    Add background styling to about page

--- frontend/src/js/page/about/About.jsx
+++ frontend/src/js/page/about/About.jsx
@@ -6,32 +6,33 @@ import { Container, Row, Col } from 'react-bootstrap'
 
 function About() {
   return (
-    <Container>
-      <Row>
-        <Col lg>
-          <b>A propos</b><hr />
-        </Col>
-      </Row>
-      <Row>
-        <Col md>
-          <p>L'E-kot est un kot à projet de l'UNamur visant à aider les étudiants rencontrant divers problèmes liés à l'informatique.</p>
-        </Col>
-        <Col md>
-          <p>
-            Ce site a donc pour but de vous aider, via sa démarche intuitive,
-            en tentant de répondre aux questions les plus fréquentes
-            (et liées à l'informatique) des étudiants de l'UNamur.
+    <div className='light-background'>
+      <Container>
+        <Row>
+          <Col lg>
+            <b>A propos</b><hr />
+          </Col>
+        </Row>
+        <Row>
+          <Col md>
+            <p>L'E-kot est un kot à projet de l'UNamur visant à aider les étudiants rencontrant divers problèmes liés à l'informatique.</p>
+          </Col>
+          <Col md>
+            <p>
+              Ce site a donc pour but de vous aider, via sa démarche intuitive,
+              en tentant de répondre aux questions les plus fréquentes
+              (et liées à l'informatique) des étudiants de l'UNamur.
           </p>
-        </Col>
-      </Row>
-      <Row>
-        <Col lg id='feedback-container'>
-          <hr />
-          <Feedbacks />
-        </Col>
-      </Row>
-
-    </Container>
+          </Col>
+        </Row>
+        <Row>
+          <Col lg id='feedback-container'>
+            <hr />
+            <Feedbacks />
+          </Col>
+        </Row>
+      </Container>
+    </div>
   )
 }
 
