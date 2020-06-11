362e006ecffa0ce448b977defd02de8fde632c0c
tsepton <tsepton@gmail.com>
2020-06-11 17:36:41 +0200

    Implement routes

--- backend/routes/feedback/feedback.js
+++ backend/routes/feedback/feedback.js
@@ -1,45 +1,32 @@
 let routes = require('express').Router()
+let feedback = require('../../lib/feedback/feedback')
 
 /* GET /?id=x,y,z */
 routes.get('/', function (req, res, next) {
-
-    // TODO : create a db youhou le fun
-    // TODO send the matching feedbacks
-    /*
-    {
-        'id' {
-            "comment": "Ceci est un commentaire",
-            "reaction": "fontawesome emoji name",
-            "date": "YYYY/MM/DD"
-        }
+    if (!req.query.id) {
+        res.status(422).json({ "message": "Query parameter 'id' is missing" })
+        return
     }
-    */
-
-    console.log( req.query.id )
-
-    const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-        Pellentesque pulvinar urna et magna congue efficitur sed dignissim leo. 
-        Proin suscipit, odio non cursus aliquam, quam erat bibendum ligula, nec 
-        commodo massa turpis eu eros. Sed eget ipsum egestas, accumsan tortor 
-        vitae, pulvinar leo. Pellentesque ut lacus id nisi fermentum convallis. 
-        Nulla non arcu a turpis vestibulum finibus. Pellentesque neque dui, 
-        egestas sit amet est id, euismod malesuada sem. Proin at suscipit urna, 
-        vel commodo mi. Etiam tincidunt eget velit id hendrerit. `
-
-    res.json({
-        'x': { "comment": 'Waw super site', "reaction": 'grin-hearts-solid', "related": 'Adresse mac' },
-        'z': { "comment": lorem, "reaction": 'sad-cry-solid', "related": 'Adresse mac' },
-        'y': { "comment": 'Merci beaucoup pour votre aide !', "reaction": 'grin-hearts-solid', "related": 'Problème internet' },
-    })
+    const ids = req.query.id.split(',')
+    let fb = new feedback()
+    let answer = fb.get(ids)
+    res.status(200).json(answer)
 })
 
 /* POST / */
 routes.post('/', function (req, res, next) {
+    if (!req.body) {
+        res.status(400).json({ "message": "json format is required" })
+        return
+    }
 
-    // TODO : create a db youhou le fun
-    console.log(req.body)
+    let fb = new feedback()
+    if (fb.save(req.body)) {
+        res.status(200)
+        return
+    }
+    res.status(422).json({ "message": "json properties are incorrect" })
 
-    res.send('Tout roule mon gars')
 })
 
 module.exports = routes;
--- backend/routes/feedback/feedbacks.js
+++ backend/routes/feedback/feedbacks.js
@@ -1,19 +1,18 @@
 let routes = require('express').Router()
+let feedback = require('../../lib/feedback/feedback')
 
 routes.get('/', function (req, res, next) {
-
     /*
     {
         'total': XX,
         'ids': ['id_1', ..., 'id_XX']
     }
     */
-    console.log(req.body)
-
-    res.json({
-        'total': 3,
-        'ids': ['id_1', 'id_2', 'id_3']
-    })
+    
+    let fb = new feedback()
+    const [total, ids] = fb.getMetadata()
+    const answer = {"total": total, "ids": ids}
+    res.status(200).json(answer)
 })
 
 module.exports = routes;
