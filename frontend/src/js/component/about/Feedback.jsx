bd2f95c0f2dc9df87cba863fad5e2099161928fa
tsepton <tsepton@gmail.com>
2020-06-14 17:36:27 +0200

    Fix date

--- frontend/src/js/component/about/Feedback.jsx
+++ frontend/src/js/component/about/Feedback.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import 'static/sass/component/about/Feedback.scss'
+import { useState } from 'react'
 
 function Feedback({ feedback }) {
+  const [date, setDate] = useState('')
+  useEffect(() => {
+    setDate(new Date(feedback.date).toString())
+  }, [])
+
   return (
     <div className='feedback'>
       <span className={`feedback-reaction ${feedback.reaction}`} />
       <div className='feedback-text'>
         <div className='feedback-comment'>{feedback.comment}</div>
-        <div className='feedback-related'>{Date(feedback.date)}</div>
+        <div className='feedback-related'>{date}</div>
       </div>
     </div>
   )
