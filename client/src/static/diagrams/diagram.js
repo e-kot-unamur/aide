a5d8a4c6442499d4ce85e1446db0e70c75794854
basilep <basile.perreaux@student.unamur.be>
2020-11-13 16:27:37 +0100

    pclent

--- client/src/static/diagrams/diagram.js
+++ client/src/static/diagrams/diagram.js
@@ -4,9 +4,11 @@ import frMac from "./2/fr.json"
 import enInternet from "./1/en.json"
 import enMac from "./2/en.json"
 
+import frPcLent from "./3/fr.json"
+
 const diagrams = {
-    "fr": [frInternet, frMac],
-    "en": [enInternet, enMac],
+    "fr": [frInternet, frMac, frPcLent],
+    "en": [enInternet, enMac, frPcLent], //TODO faire pclent en anglais 
 }
 
 export default diagrams
