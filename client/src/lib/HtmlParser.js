9a73537955388a296a22682e8b5c218a6bda947f
Thibaut <tsepton@gmail.com>
2022-04-01 11:23:21 +0200

    Add new methods

--- client/src/lib/HtmlParser.js
+++ client/src/lib/HtmlParser.js
@@ -1,6 +1,23 @@
-const contact = 'https://www.messenger.com/t/ekotnamur'
+const contact = 'https://www.facebook.com/ekotnamur'
+const facebookLink = 'https://www.messenger.com/t/ekotnamur'
+const instagramLink = 'https://www.instagram.com/ekot.unamur/'
 
 export default class HtmlParser {
+
+    // TODO refactor the following
+
+    static parseFacebook(text) {
+        return text
+            .replace("<facebook>", `<a href=${facebookLink} target="_blank" rel="noopener">`)
+            .replace("</facebook>", `</a>`)
+    }
+
+    static parseInstagramTag(text) {
+        return text
+            .replace("<instagram>", `<a href=${instagramLink} target="_blank" rel="noopener">`)
+            .replace("</instagram>", `</a>`)
+    }
+
     /**
      * replace <contact> from the json diagram to a 
      * <a> tag referencing messenger's contact page
