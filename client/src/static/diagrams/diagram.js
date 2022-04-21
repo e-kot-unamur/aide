fc0efe68e05293bf137612dd71c4df1a8e02ac0a
Pierre Luycx <pierre.luycx@gmail.com>
2022-04-21 10:21:34 +0200

    Add password update diagram

--- client/src/static/diagrams/diagram.js
+++ client/src/static/diagrams/diagram.js
@@ -1,19 +1,17 @@
 import frInternet from "./1/fr.json"
 import enInternet from "./1/en.json"
-
 import frMac from "./2/fr.json"
 import enMac from "./2/en.json"
-
 import frPcLent from "./3/fr.json"
 import enPcLent from "./3/en.json"
-
 import frWifi from "./4/fr.json"
 import enWifi from "./4/en.json"
-// TODO import english translation
+import frPasswordUpdate from "./5/fr.json"
+import enPasswordUpdate from "./5/en.json"
 
 const diagrams = {
-    "fr": [frInternet, frMac, frPcLent, frWifi],
-    "en": [enInternet, enMac, enPcLent, enWifi],
+    "fr": [frPasswordUpdate, frInternet, frMac, frPcLent, frWifi],
+    "en": [enPasswordUpdate, enInternet, enMac, enPcLent, enWifi],
 }
 
 export default diagrams
